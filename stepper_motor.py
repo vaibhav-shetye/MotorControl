aa6d476e9418dd84f6a4022952713aab4ce1337f
vaibhav-shetye <vaibhav.shetyeg@gmail.com>
2018-09-10 19:49:10 +0530

    Updated Half step and full step functions.

--- stepper_motor.py
+++ stepper_motor.py
@@ -1,25 +1,30 @@
 from machine import Pin
+import time
 
 
 class StepperMotor(object):
     def __init__(self, p1, p2, p3, p4):
+        self.debug = False
+        self.half_step_delay = 2
+        self.full_step_delay = 10
         #self.MODEL = model
         self.pins = list()
         self.pins.append(Pin(p1, Pin.OUT))
         self.pins.append(Pin(p2, Pin.OUT))
         self.pins.append(Pin(p3, Pin.OUT))
         self.pins.append(Pin(p4, Pin.OUT))
-        self.signal_stepper(0,0,0,0)
+        self.signal_stepper(0, 0, 0, 0)
 
     def signal_stepper(self, *signals):
+        if self.debug:
+            print(self.pins, ":", signals)
         for p, s in zip(self.pins, signals):
-            print(p, " : ", s, end=', ')
             if s == 0:
                 p.off()
             else:
                 p.on()
 
-    def half_step(self):
+    def half_step(self, direction=1):
         HALF_STEP = [
             [0, 0, 0, 1],
             [0, 0, 1, 1],
@@ -30,18 +35,21 @@ class StepperMotor(object):
             [1, 0, 0, 0],
             [1, 0, 0, 1],
         ]
-        for step in HALF_STEP:
-            self.signal_stepper(*step)
+        self._take_step(HALF_STEP, direction, self.half_step_delay)
 
-    def full_step(self):
+    def full_step(self, direction=1):
         FULL_STEP = [
             [1, 0, 1, 0],
             [0, 1, 1, 0],
             [0, 1, 0, 1],
             [1, 0, 0, 1],
         ]
-        for step in FULL_STEP:
-            self.signal_stepper(*step)
+        self._take_step(FULL_STEP, direction, self.full_step_delay)
+
+    def _take_step(self, step_matrix, direction, delay):
+        for step in step_matrix:
+            self.signal_stepper(*step[::direction])
+            time.sleep_ms(delay)
 
 
 if __name__ == "__main__":
